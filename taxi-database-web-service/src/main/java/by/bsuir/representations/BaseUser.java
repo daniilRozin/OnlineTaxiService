28dd64e2f7765dc82a3f0b5896878c291f1e55b1
andrey <andrey@andrey-PC>
2017-03-22 22:51:21 +0300

    Initial commit which consist one bean for base user

---
+++ taxi-database-web-service/src/main/java/by/bsuir/representations/BaseUser.java
@@ -0,0 +1,86 @@
+package by.bsuir.representations;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class BaseUser {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+    @Column(name = "login")
+	private String login;
+	
+	private String password;
+	@NotNull
+    @Column(name = "email")
+	private String email;
+	@NotNull
+    @Column(name = "first_name")
+	private String firstName;
+	@NotNull
+    @Column(name = "second_name")
+	private String secondName;
+
+	public BaseUser() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	@NotNull
+    @Column(name = "password")
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getsecondName() {
+		return secondName;
+	}
+
+	public void setsecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+}
